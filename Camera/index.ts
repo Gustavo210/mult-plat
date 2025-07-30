499159632be9f5590740700c83e1b1f7651c1b3c
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2025-07-30 09:13:37 -0300

    Altera localização do arquivo de entrada da lib

--- Camera/index.ts
+++
@@ -1,8 +0,0 @@
-import { CameraRoot } from "./src/components/CameraRoot";
-import { Header } from "./src/components/Header";
-import { Overlay } from "./src/components/Overlay";
-
-export const Camera = Object.assign(CameraRoot, {
-  Header,
-  Overlay,
-});

0d68df80dfef7c93f1d10d4a9b23ffed2bd32074
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2025-08-04 16:32:48 -0300

    Simula em versão android sub header do meu look

--- app/test-page/index.android.tsx
+++ app/test-page/index.android.tsx
@@ -5,11 +5,48 @@ import { Typography } from "@mobilestock-native/typography";
 export default function IndexAndroid() {
   return (
     <>
-      <SubHeader.Horizontal>
-        <SubHeader.Button icon="Cart" text="Carrinho" />
-        <SubHeader.Button icon="TruckFastOutline" text="Entregas" />
-        <SubHeader.Button icon="AttachMoney" text="LookPay" />
-        <SubHeader.Button icon="CreditCard" text="Cartão" />
+      <SubHeader.Horizontal align="BETWEEN_CENTER" overflow="SCROLL">
+        <Container.Vertical>
+          <SubHeader.Button
+            icon="Box"
+            text="Produtos"
+            iconAlign="BEFORE-TEXT"
+            variant="TRANSPARENT"
+          />
+          <SubHeader.Button
+            icon="CreditCard"
+            text="LookPay"
+            iconAlign="BEFORE-TEXT"
+            variant="TRANSPARENT"
+          />
+          <SubHeader.Button
+            icon="Cart"
+            text="Carrinho"
+            iconAlign="BEFORE-TEXT"
+            notification={4}
+            variant="TRANSPARENT"
+          />
+        </Container.Vertical>
+        <Container.Vertical>
+          <SubHeader.Button
+            icon="UserOutline"
+            text="Perfil"
+            iconAlign="BEFORE-TEXT"
+            variant="TRANSPARENT"
+          />
+          <SubHeader.Button
+            icon="Whatsapp"
+            text="Dúvidas"
+            iconAlign="BEFORE-TEXT"
+            variant="TRANSPARENT"
+          />
+          <SubHeader.Button
+            icon="Settings"
+            text="Configurações"
+            iconAlign="BEFORE-TEXT"
+            variant="TRANSPARENT"
+          />
+        </Container.Vertical>
       </SubHeader.Horizontal>
       <Container.Vertical>
         <Typography>Rota Android para teste</Typography>

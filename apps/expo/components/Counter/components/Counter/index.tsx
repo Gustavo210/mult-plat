2d0b70a76ce48548b7430a4d0a13a03249791194
Gustavo210 <gustavogontijoo@gmail.com>
2025-10-07 11:14:07 -0300

    feat: criando componente de exibir a contagem

---
+++ apps/expo/components/Counter/components/Counter/index.tsx
@@ -0,0 +1,15 @@
+import { Container } from "@mobilestock-native/container";
+import { Typography } from "@mobilestock-native/typography";
+import { useCounter } from "../../hooks/useCount";
+
+export function Counter() {
+  const { count } = useCounter();
+  return (
+    <Container.Vertical
+      style={{ minWidth: 40, userSelect: "none" }}
+      align="CENTER"
+    >
+      <Typography>{count}</Typography>
+    </Container.Vertical>
+  );
+}

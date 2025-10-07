9bc26075170de5e44b659fa0bf7012d9f5eae2cb
Gustavo210 <gustavogontijoo@gmail.com>
2025-10-07 11:15:47 -0300

    refactor: ajustando entripoint do componente

--- apps/expo/components/Counter/index.tsx
+++ apps/expo/components/Counter/index.tsx
@@ -1,60 +1,28 @@
-import { Button } from "@mobilestock-native/button";
-import { Container } from "@mobilestock-native/container";
-import { Typography } from "@mobilestock-native/typography";
-import { useState } from "react";
-import styled from "styled-components/native";
+import { Button } from "./components/Button";
+import { Container } from "./components/Container";
+import { Counter as CounterRaw } from "./components/Counter";
+import { CounterProvider } from "./hooks/useCount";
 
-export function Counter({
-  variant = "read-only",
-}: {
-  variant?: "editable" | "read-only" | "grouped";
-}) {
-  const [count, setCount] = useState(0);
+interface CounterProps {
+  multiplier?: number;
+  initialCount?: number;
+  maxCount?: number;
+  minCount?: number;
+  editable?: boolean;
+}
+
+export function Counter({ initialCount, maxCount, minCount }: CounterProps) {
   return (
-    <Container.Horizontal
-      align="CENTER"
-      gap="XS"
-      style={{ userSelect: "none" }}
+    <CounterProvider
+      initialCount={initialCount}
+      maxCount={maxCount}
+      minCount={minCount}
     >
-      <Button
-        size="SM"
-        icon="Minus"
-        onPress={() => setCount(count - 1)}
-        onLongPress={() => setCount(count - 10)}
-      />
-      <Container.Vertical
-        style={{ minWidth: 40, userSelect: "none" }}
-        align="CENTER"
-      >
-        {variant === "editable" ? (
-          <Input
-            value={String(count)}
-            onChangeText={(text) => {
-              const parsed = parseInt(text, 10);
-              if (!isNaN(parsed)) {
-                setCount(parsed);
-              } else {
-                setCount(0);
-              }
-            }}
-            keyboardType="decimal-pad"
-          />
-        ) : null}
-        {variant === "read-only" ? <Typography>{count}</Typography> : null}
-      </Container.Vertical>
-      <Button
-        size="SM"
-        icon="Plus"
-        onPress={() => setCount(count + 1)}
-        onLongPress={() => setCount(count + 10)}
-      />
-    </Container.Horizontal>
+      <Container>
+        <Button type="MINUS" />
+        <CounterRaw />
+        <Button type="PLUS" />
+      </Container>
+    </CounterProvider>
   );
 }
-const Input = styled.TextInput`
-  padding: 4px 8px;
-  border-radius: 4px;
-  border: 1px solid gray;
-  width: 100%;
-  text-align: center;
-`;

0396b72821399e7f7a7fe42f269784268aa1094e
Gustavo210 <gustavogontijoo@gmail.com>
2025-10-07 14:11:13 -0300

    feature: criando componente de badge

---
+++ apps/expo/components/Counter/components/Badge/index.tsx
@@ -0,0 +1,5 @@
+import { Typography, TypographyProps } from "@mobilestock-native/typography";
+
+export function Badge({ children }: TypographyProps) {
+  return <Typography>{children}</Typography>;
+}

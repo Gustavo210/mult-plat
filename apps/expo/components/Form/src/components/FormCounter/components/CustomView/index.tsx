8883e260435e74eb122a3a463b4d2611daf7b8d9
Gustavo210 <gustavogontijoo@gmail.com>
2025-10-08 10:01:39 -0300

    ajustando CustomView

--- apps/expo/components/Form/src/components/FormCounter/components/CustomView/index.tsx
+++ apps/expo/components/Form/src/components/FormCounter/components/CustomView/index.tsx
@@ -7,10 +7,15 @@ import {
   ReactNode,
   useCallback,
 } from "react";
+import { useCounter } from "../../hooks/useCount";
 import { Badge } from "../Badge";
 import { ContainerPill } from "../ContainerPiil";
+import { Error } from "../Error";
+import { Label } from "../Label";
 
 export function CustomView({ children }: { children: React.ReactNode }) {
+  const { label, error, labelPosition } = useCounter();
+
   const externalChildren: ReactNode[] = [];
   const internalChildren: ReactNode[] = [];
 
@@ -37,14 +42,26 @@ export function CustomView({ children }: { children: React.ReactNode }) {
   });
 
   return (
-    <Container.Vertical>
-      <Container.Horizontal
-        gap="XS"
-        align={externalChildren.length ? "CENTER_START" : "CENTER"}
-      >
-        {externalChildren}
-        <ContainerPill>{internalChildren}</ContainerPill>
-      </Container.Horizontal>
+    <Container.Vertical align="CENTER_START">
+      {labelPosition === "TOP_START" && label && (
+        <Container.Horizontal gap="XS">
+          <Label>{label}</Label>
+          {error && !externalChildren.length && <Error>{error}</Error>}
+        </Container.Horizontal>
+      )}
+      <Container.Vertical align="CENTER">
+        {labelPosition !== "TOP_START" && label && <Label>{label}</Label>}
+        {labelPosition !== "TOP_START" && error && !externalChildren.length && (
+          <Error>{error}</Error>
+        )}
+        <Container.Horizontal gap="XS" align={"CENTER_START"}>
+          {externalChildren}
+          <Container.Vertical>
+            <ContainerPill>{internalChildren}</ContainerPill>
+            {error && !!externalChildren.length && <Error>{error}</Error>}
+          </Container.Vertical>
+        </Container.Horizontal>
+      </Container.Vertical>
     </Container.Vertical>
   );
 }

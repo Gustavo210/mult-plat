8b551a8d10e604456251384d95b211f6f2f244b6
Gustavo210 <gustavogontijoo@gmail.com>
2025-11-04 17:55:00 -0300

    removendo alteracoes

--- apps/next-app/components/Form/src/components/FormCounter/components/Label/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Typography, TypographyProps } from "@mobilestockweb/typography";
-
-export function Label({ children }: TypographyProps) {
-  return <Typography weight="REGULAR">{children}</Typography>;
-}
--- apps/next-app/components/Form/src/components/FormVertical/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Container, ViewBaseProps } from '@mobilestockweb/container'
-
-export function FormVertical({ children, ...props }: ViewBaseProps) {
-  return (
-    <Container.Vertical gap="SM" {...props}>
-      {children}
-    </Container.Vertical>
-  )
-}

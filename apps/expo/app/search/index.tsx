a38091951075493b6e7fafdcfe952b15d173fcc1
Gustavo210 <gustavogontijoo@gmail.com>
2025-12-17 19:16:28 -0300

    search

--- apps/expo/app/search/index.tsx
+++ apps/expo/app/search/index.tsx
@@ -2,87 +2,95 @@ import { Container } from "@mobilestock-native/container";
 import { Typography } from "@mobilestock-native/typography";
 import { Search } from "../../components/Search/src";
 
+const data = [
+  {
+    pessoas: ["Pessoa A", "Pessoa B"],
+    batata: {
+      cebola: {
+        teste: "Teste profundo",
+      },
+    },
+    batatinha: [
+      {
+        tipo: "Tipo 1",
+        sabor: "Sabor A",
+      },
+      {
+        tipo: "Tipo 2",
+        sabor: "Sabor B",
+      },
+    ],
+    teste: "Sugestao 1",
+    valor: 123,
+    nulo: null,
+  },
+  {
+    pessoas: ["Pessoa 1", "Pessoa 2"],
+    batata: {
+      cebola: {
+        teste: "Outro teste profundo",
+      },
+    },
+    batatinha: [
+      {
+        tipo: "Tipo 3",
+        sabor: "Sabor C",
+      },
+      {
+        tipo: "Tipo 4",
+        sabor: "Sabor D",
+      },
+    ],
+    teste: "Sugestao 2",
+    valor: 456,
+    nulo: null,
+  },
+  {
+    pessoas: ["Pessoa 3", "Pessoa 4"],
+    batata: {
+      cebola: {
+        teste: "Mais um teste profundo",
+      },
+    },
+    batatinha: [
+      {
+        tipo: "Tipo 5",
+        sabor: "Sabor E",
+      },
+      {
+        tipo: "Tipo 6",
+        sabor: "Sabor F",
+      },
+    ],
+    teste: "Sugestao 3",
+    valor: 789,
+    nulo: null,
+  },
+];
+
 export default function Index() {
   return (
     <Container.Vertical>
       <Typography>Rota para search</Typography>
       <Search
-        defaultData={[
-          {
-            pessoas: ["Pessoa A", "Pessoa B"],
-            batata: {
-              cebola: {
-                teste: "Teste profundo",
-              },
-            },
-            batatinha: [
-              {
-                tipo: "Tipo 1",
-                sabor: "Sabor A",
-              },
-              {
-                tipo: "Tipo 2",
-                sabor: "Sabor B",
-              },
-            ],
-            teste: "Sugestao 1",
-            valor: 123,
-            nulo: null,
-          },
-          {
-            pessoas: ["Pessoa 1", "Pessoa 2"],
-            batata: {
-              cebola: {
-                teste: "Outro teste profundo",
-              },
-            },
-            batatinha: [
-              {
-                tipo: "Tipo 3",
-                sabor: "Sabor C",
-              },
-              {
-                tipo: "Tipo 4",
-                sabor: "Sabor D",
-              },
-            ],
-            teste: "Sugestao 2",
-            valor: 456,
-            nulo: null,
-          },
-          {
-            pessoas: ["Pessoa 3", "Pessoa 4"],
-            batata: {
-              cebola: {
-                teste: "Mais um teste profundo",
-              },
-            },
-            batatinha: [
-              {
-                tipo: "Tipo 5",
-                sabor: "Sabor E",
-              },
-              {
-                tipo: "Tipo 6",
-                sabor: "Sabor F",
-              },
-            ],
-            teste: "Sugestao 3",
-            valor: 789,
-            nulo: null,
-          },
-        ]}
-        valueSuggestionKey="batatinha[*].sabor"
-        variant="no-button"
+        // defaultData={data}
+        // nao renderizar suggestions
+        valueSuggestionKey="batata.cebola.teste"
+        variant="attached"
+        iconSearchButton="ArrowRight"
         fetchOnQuery={async (query, signal) => {
-          await new Promise((resolve) => setTimeout(resolve, 5000));
-          return [];
+          return new Promise<typeof data>((resolve, reject) => {
+            const timeout = setTimeout(() => {
+              resolve(data);
+            }, 2000);
+
+            signal?.addEventListener("abort", () => {
+              clearTimeout(timeout);
+              reject();
+            });
+          });
         }}
-        // defaultData={["Nova sugestão 1", "Nova sugestão 2", "Nova sugestão 3"]}
       />
-      {/* <Form>
-        <Form.Input label="Search Input" name="searchInput" />
-      </Form> */}
     </Container.Vertical>
   );
 }

7ccb789168b8b6579070a70ef5e9546a29d42ade
Gustavo210 <gustavogontijoo@gmail.com>
2025-12-17 17:30:59 -0300

    adicionando botao de X

--- apps/expo/components/Search/src/components/Input/index.tsx
+++ apps/expo/components/Search/src/components/Input/index.tsx
@@ -1,9 +1,25 @@
+import { Clickable } from "@mobilestock-native/clickable";
 import { Container } from "@mobilestock-native/container";
+import { Icon } from "@mobilestock-native/icons";
+import { useRef, useState } from "react";
 import { ActivityIndicator, TextInput } from "react-native";
 import { useSearch } from "../../hooks/useSearch";
 
 export function Input() {
   const Search = useSearch();
+  const [showXButton, setShowXButton] = useState(false);
+  const inputRef = useRef<TextInput>(null);
+
+  function clearInput() {
+    if (inputRef.current) {
+      Search.clearResults();
+      Search.cancelOngoingRequest();
+      inputRef.current.clear();
+      setShowXButton(false);
+      Search.debounceSearch("");
+    }
+  }
+
   return (
     <Container.Horizontal
       full
@@ -11,18 +27,30 @@ export function Input() {
         backgroundColor: "white",
       }}
       padding="NONE_XS"
+      gap="XS"
     >
       <TextInput
+        ref={inputRef}
         style={{
           height: 40,
           width: "100%",
           outline: "none",
         }}
-        onChangeText={Search.debounceSearch}
+        onChangeText={(text) => {
+          setShowXButton(text.length > 0);
+          Search.debounceSearch(text);
+        }}
         placeholder="Digite"
-        clearButtonMode="always"
+        clearButtonMode="never"
       />
       {Search.isLoading && <ActivityIndicator color={"#cecece"} />}
+      {showXButton && (
+        <Container.Horizontal align="CENTER">
+          <Clickable onPress={clearInput}>
+            <Icon name="X" size="XS" />
+          </Clickable>
+        </Container.Horizontal>
+      )}
     </Container.Horizontal>
   );
 }

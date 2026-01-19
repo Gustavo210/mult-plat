c980b6cb8e370e493bf7f2d40c1ed23f321e1136
Gustavo210 <gustavogontijoo@gmail.com>
2026-01-19 09:21:53 -0300

    chore: o item selecionado fica no input

--- apps/expo/components/Search/src/components/Input/index.tsx
+++ apps/expo/components/Search/src/components/Input/index.tsx
@@ -1,23 +1,29 @@
 import { Clickable } from "@mobilestock-native/clickable";
 import { Container } from "@mobilestock-native/container";
 import { Icon } from "@mobilestock-native/icons";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ActivityIndicator, TextInput } from "react-native";
 import { useSearch } from "../../hooks/useSearch";
 
 export function Input({ hiddenLoadingIndicator = false }) {
   const Search = useSearch();
   const [showXButton, setShowXButton] = useState(false);
+  const [inputValue, setInputValue] = useState("");
   const inputRef = useRef<TextInput>(null);
 
-  function clearInput() {
-    if (inputRef.current) {
-      Search.clearResults();
-      Search.cancelOngoingRequest();
-      inputRef.current.clear();
-      setShowXButton(false);
-      Search.debounceSearch("");
+  useEffect(() => {
+    if (Search.selectedItem) {
+      setInputValue(String(Search.selectedItem));
+      setShowXButton(true);
     }
+  }, [Search.selectedItem]);
+
+  function clearInput() {
+    Search.clearResults();
+    Search.cancelOngoingRequest();
+    setInputValue("");
+    setShowXButton(false);
+    Search.debounceSearch("");
   }
 
   return (
@@ -31,18 +37,22 @@ export function Input({ hiddenLoadingIndicator = false }) {
     >
       <TextInput
         ref={inputRef}
+        value={inputValue}
+        selectTextOnFocus={!!Search.selectedItem}
         style={{
           height: 40,
           width: "100%",
           outline: "none",
         }}
         onChangeText={(text) => {
+          setInputValue(text);
           setShowXButton(text.length > 0);
           if (Search.searchWhenTyping) {
             Search.debounceSearch(text);
           } else {
             Search.inputContentRef.current = text;
           }
+          Search.configureSelectedItem(null);
         }}
         placeholder="Digite"
         clearButtonMode="never"
